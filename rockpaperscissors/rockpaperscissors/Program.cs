11ede049b1f5127e43352158109a020a8232d759
manzilat <amtulnoorq@yahoo.com>
2018-09-21 14:14:28 -0500

    written if else statement for lizard and spock

--- rockpaperscissors/rockpaperscissors/Program.cs
+++ rockpaperscissors/rockpaperscissors/Program.cs
@@ -10,11 +10,11 @@ namespace rockpaperscissors
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Do you choose rock,paper,scissors,lizard,spock");
+            Console.WriteLine("please select any one of the following\n rock\n paper\n scissors\n lizard\n spock");
             string userChoice = Console.ReadLine();
 
             Random r = new Random();
-            int computerChoice = r.Next(4);
+            int computerChoice = r.Next(6);
 
             if (computerChoice == 1)
             {
@@ -57,28 +57,28 @@ namespace rockpaperscissors
                 if (userChoice == "rock")
                 {
                     Console.WriteLine("The computer chose paper");
-                    Console.WriteLine("Sorry you lose,paper beat rock");
+                    Console.WriteLine("Sorry you lose,paper covers rock");
 
                 }
                 else if (userChoice == "paper")
                 {
                     Console.WriteLine("The computer chose scissors");
-                    Console.WriteLine("Sorry you lose,scissors beat paper ");
+                    Console.WriteLine("Sorry you lose,Scissors cuts Paper ");
 
                 }
                 else if (userChoice == "scissors")
                 {
-                    Console.WriteLine("The computer chose rock");
-                    Console.WriteLine("Sorry you lose,rock beats scissors");
+                    Console.WriteLine("The computer chose lizard");
+                    Console.WriteLine("Sorry you lose,Scissors decapitates Lizard");
                 }
                 else if (userChoice == "lizard")
                 {
-                    Console.WriteLine("The computer chose lizard");
-                    Console.WriteLine("Sorry you lose,Rock crushes Lizard");
+                    Console.WriteLine("The computer chose spock");
+                    Console.WriteLine("Sorry you lose,Lizard poisons Spock");
                 }
                 else if (userChoice == "spock")
                 {
-                    Console.WriteLine("The computer chose spock");
+                    Console.WriteLine("The computer chose rock");
                     Console.WriteLine("Sorry you lose,Spock vaporizes Rock ");
                 }
                 else
@@ -91,41 +91,121 @@ namespace rockpaperscissors
                 if (userChoice == "rock")
                 {
                     Console.WriteLine("The computer chose scissors");
-                    Console.WriteLine("You win,rock beats scissors");
+                    Console.WriteLine("You win,rock crushes scissors");
 
                 }
                 else if (userChoice == "paper")
                 {
-                    Console.WriteLine("The computer chose rock");
-                    Console.WriteLine("You win,paper beats rock");
+                    Console.WriteLine("The computer chose lizard");
+                    Console.WriteLine("You lose,Lizard eats Paper");
 
                 }
                 else if (userChoice == "scissors")
                 {
-                    Console.WriteLine("The computer chose paper");
-                    Console.WriteLine("You win,scissors beat paper");
+                    Console.WriteLine("The computer chose spock");
+                    Console.WriteLine("You lose,Spock smashes Scissors");
 
                 }
                 else if (userChoice == "lizard")
                 {
-                    Console.WriteLine("The computer chose lizard");
-                    Console.WriteLine("You win,Scissors decapitates Lizard");
+                    Console.WriteLine("The computer chose rock");
+                    Console.WriteLine("You win,Rock crushes Lizard");
 
                 }
                 else if (userChoice == "spock")
                 {
                     Console.WriteLine("The computer chose paper");
-                    Console.WriteLine("You win,scissors beat paper");
+                    Console.WriteLine("You lose,Paper disproves Spock");
 
                 }
                 else
                 {
-                    Console.WriteLine("You must choose rock,paper or scissors!");
+                    Console.WriteLine("You must choose rock,paper,scissors,lizard or spock!");
+
+                }
+
+            }
+           else if (computerChoice == 4)
+            {
+                if (userChoice == "rock")
+                {
+                    Console.WriteLine("The computer chose lizard");
+                    Console.WriteLine("You win,rock crushes lizard");
+
+                }
+                else if (userChoice == "paper")
+                {
+                    Console.WriteLine("The computer chose spock");
+                    Console.WriteLine("You win,Paper disproves Spock");
+
+                }
+                else if (userChoice == "scissors")
+                {
+                    Console.WriteLine("The computer chose rock");
+                    Console.WriteLine("You lose,rock crushes Scissors");
+
+                }
+                else if (userChoice == "lizard")
+                {
+                    Console.WriteLine("The computer chose paper");
+                    Console.WriteLine("You win,Lizard eats Paper");
+
+                }
+                else if (userChoice == "spock")
+                {
+                    Console.WriteLine("The computer chose scissors");
+                    Console.WriteLine("You win,Spock smashes Scissors");
+
+                }
+                else
+                {
+                    Console.WriteLine("You must choose rock,paper,scissors,lizard or spock!");
 
                 }
 
             }
 
+
+
+            else if (computerChoice == 5)
+            {
+                if (userChoice == "rock")
+                {
+                    Console.WriteLine("The computer chose spock");
+                    Console.WriteLine("You win,Spock vaporizes Rock ");
+
+                }
+                else if (userChoice == "paper")
+                {
+                    Console.WriteLine("The computer chose rock");
+                    Console.WriteLine("You win,Paper covers rock");
+
+                }
+                else if (userChoice == "scissors")
+                {
+                    Console.WriteLine("The computer chose paper");
+                    Console.WriteLine("You win,scissors cut rock");
+
+                }
+                else if (userChoice == "lizard")
+                {
+                    Console.WriteLine("The computer chose scissors");
+                    Console.WriteLine("You lose,Scissors decapitates Lizard");
+
+                }
+                else if (userChoice == "spock")
+                {
+                    Console.WriteLine("The computer chose lizard");
+                    Console.WriteLine("You lose,Lizard poisons Spock");
+
+                }
+                else
+                {
+                    Console.WriteLine("You must choose rock,paper,scissors,lizard or spock!");
+
+                }
+
+            }
             bool keepPlaying = true;
             while (keepPlaying)
             {
