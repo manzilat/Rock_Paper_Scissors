4934d5ab073b9816df4bbbdc8ffe56e326b2473f
manzilat <amtulnoorq@yahoo.com>
2018-09-21 11:56:14 -0500

    successful getting the output for the game but for 1 round

--- rockpaperscissors/rockpaperscissors/Program.cs
+++ rockpaperscissors/rockpaperscissors/Program.cs
@@ -6,40 +6,135 @@ using System.Threading.Tasks;
 
 namespace rockpaperscissors
 {
-    class RPSLSGame
+    class Program
     {
         static void Main(string[] args)
         {
-            RPSLSGame rpslsGame = new RPSLSGame();
-            Console.WriteLine("Welcome to Rock, Paper, Scissors, Lizard, Spock!!");
-           
-            
-            Console.WriteLine("Would you like to play? Enter 'yes', 'y', 'Yes', or 'Y' for yes.\nEnter 'no', 'No', 'n', 'N' for no.\nDecision?");
-            string playerChoice = Console.ReadLine();
-            if (playerChoice == "yes" || playerChoice == "y" || playerChoice == "Yes" || playerChoice == "Y")
+            Console.WriteLine("Do you choose rock,paper,scissors,lizard,spock");
+            string userChoice = Console.ReadLine();
+
+            Random r = new Random();
+            int computerChoice = r.Next(4);
+
+            if (computerChoice == 1)
             {
-                //calling method to start game
-                rpslsGame.StartGame();
-                //calling method to display results of game
-                rpslsGame.DetermineWinner();
+                if (userChoice == "rock")
+                {
+                    Console.WriteLine("The computer chose rock");
+                    Console.WriteLine("It is a tie ");
+                }
+                else if (userChoice == "paper")
+                {
+                    Console.WriteLine("The computer chose paper");
+                    Console.WriteLine("It is a tie ");
+
+                }
+                else if (userChoice == "scissors")
+                {
+                    Console.WriteLine("The computer chose scissors");
+                    Console.WriteLine("It is a tie ");
+                }
+                else if (userChoice == "lizard")
+                {
+                    Console.WriteLine("The computer chose lizard");
+                    Console.WriteLine("It is a tie ");
+                }
+                else if (userChoice == "spock")
+                {
+                    Console.WriteLine("The computer chose spock");
+                    Console.WriteLine("It is a tie ");
+                }
+                else
+                {
+                    Console.WriteLine("You must choose rock,paper,scissors,lizard,spock!");
+
+                }
+
             }
 
-            else if (playerChoice == "no" || playerChoice == "n" || playerChoice == "No" || playerChoice == "N")
+            else if (computerChoice == 2)
             {
-                Console.WriteLine("See ya!");
+                if (userChoice == "rock")
+                {
+                    Console.WriteLine("The computer chose paper");
+                    Console.WriteLine("Sorry you lose,paper beat rock");
+
+                }
+                else if (userChoice == "paper")
+                {
+                    Console.WriteLine("The computer chose scissors");
+                    Console.WriteLine("Sorry you lose,scissors beat paper ");
+
+                }
+                else if (userChoice == "scissors")
+                {
+                    Console.WriteLine("The computer chose rock");
+                    Console.WriteLine("Sorry you lose,rock beats scissors");
+                }
+                else if (userChoice == "lizard")
+                {
+                    Console.WriteLine("The computer chose lizard");
+                    Console.WriteLine("Sorry you lose,Rock crushes Lizard");
+                }
+                else if (userChoice == "spock")
+                {
+                    Console.WriteLine("The computer chose spock");
+                    Console.WriteLine("Sorry you lose,Spock vaporizes Rock ");
+                }
+                else
+                {
+                    Console.WriteLine("You must choose rock,paper or scissors,lizard,spock!");
+                }
+            }
+            else if (computerChoice == 3)
+            {
+                if (userChoice == "rock")
+                {
+                    Console.WriteLine("The computer chose scissors");
+                    Console.WriteLine("You win,rock beats scissors");
+
+                }
+                else if (userChoice == "paper")
+                {
+                    Console.WriteLine("The computer chose rock");
+                    Console.WriteLine("You win,paper beats rock");
+
+                }
+                else if (userChoice == "scissors")
+                {
+                    Console.WriteLine("The computer chose paper");
+                    Console.WriteLine("You win,scissors beat paper");
+
+                }
+                else if (userChoice == "lizard")
+                {
+                    Console.WriteLine("The computer chose lizard");
+                    Console.WriteLine("You win,Scissors decapitates Lizard");
+
+                }
+                else if (userChoice == "spock")
+                {
+                    Console.WriteLine("The computer chose paper");
+                    Console.WriteLine("You win,scissors beat paper");
+
+                }
+                else
+                {
+                    Console.WriteLine("You must choose rock,paper or scissors!");
+
+                }
+
             }
 
-            else
+            bool keepPlaying = true;
+            while (keepPlaying)
             {
-                Console.WriteLine("Invalid choice.");
-                Console.WriteLine("Yes or no?");
-                playerChoice = Console.ReadLine();
+                //game logic here
+
+                Console.WriteLine("New game? y/n");
+                ConsoleKeyInfo cki = Console.ReadKey(); //wait for player to press a key
+                keepPlaying = cki.KeyChar == 'y'; //continue only if y was pressed
             }
-
-            //beginning game again.
-            rpslsGame.PlayAgain();
-
-
         }
     }
 }
